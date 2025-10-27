23f55a3802ab514f41c63712cdcd0572c1db3019
Eliot Dupouy <35254392+eliotdupouy@users.noreply.github.com>
2025-10-27 18:35:26 +0100

    Add Vite preview scaffolding

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FootballManagerLite from './game.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FootballManagerLite />
+  </React.StrictMode>
+);
